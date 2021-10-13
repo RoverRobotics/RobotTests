317457df2e4859895db92f9fcd9ff3ca3357fb8c
drhieu <will@roverrobotics.com>
2021-10-13 12:50:52 -0500

    init

---
+++ DataCollector.py
@@ -0,0 +1,17 @@
+class DataCollector:
+    def __init__(self,ip) :
+        self.ip_address = ip
+        self.data # return numpy array of data (N, data)
+        pass
+    def start():
+        '''connect the microcontroller and start data collection'''
+        pass
+    def stop():
+        '''stop data collection'''
+        pass
+    def save_to_file(path):
+        '''save collected data for later processing or inspection'''
+        pass
+    def load_from_file(path):
+        '''load saved data'''
+        pass
